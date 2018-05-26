389b0e0bed1c7749c2698760afa6ffdcd58e043a
vjerksen <md040207@naver.com>
2018-05-26 14:06:23 +0900

    Add class "Graphics"

--- interior/Program.cs
+++ interior/Program.cs
@@ -18,6 +18,13 @@ namespace interior
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());
+
+            
+
+
+            
         }
     }
 }
+
+
